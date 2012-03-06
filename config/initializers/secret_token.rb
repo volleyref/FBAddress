647133b4b1793c307755b7f4d25259cb39f294a4
Sung Baek <volleyref@gmail.com>
2012-03-06 10:58:03 -0800

    Generated FBAddress app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FBAddress::Application.config.secret_token = '1a11b9e270ee117c6e967bb05687a241db45a4593e9c8ade66c0cf18144bbbaac6dd0be1c19119cb40c38ea41f75e635d247b7060419f4f326bb0efc3da95517'
